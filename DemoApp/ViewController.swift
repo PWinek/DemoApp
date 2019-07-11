d495106ea7295833633ef38d21d8d2b9c85a85c9
Amelia Płatek <amelia.platek@gmail.com>
2019-07-11 19:15:02 +0200

    Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Amelia Płatek on 11/07/2019.
+//  Copyright © 2019 paw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
